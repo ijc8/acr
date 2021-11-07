a18d55c35325207b488af2785f9754cd1b38c0c7
Ian Clester <ijc@ijc8.me>
2021-11-07 00:37:04 -0400

    Plot confuson matrices; WIP on  strokes.

--- evaluate.py
+++ evaluate.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.io.wavfile
 import scipy.signal
 
+from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split, LeaveOneGroupOut
 from sklearn.pipeline import make_pipeline
 
@@ -25,8 +26,8 @@ def run(preprocessor, classifier, seed=None):
 
     # Avoid re-doing preprocessing for each of these tasks.
     X = preprocessor(letters.reshape(-1), fs)
-    y = np.indices(letters.shape[:3])[1].reshape(-1)
-    subjects = np.indices(letters.shape[:3])[0].reshape(-1)
+    y = np.indices(letters.shape)[1].reshape(-1)
+    subjects = np.indices(letters.shape)[0].reshape(-1)
 
     # Single-subject accuracy (fully personalized mode)
     for subject in range(len(letters)):
@@ -36,7 +37,9 @@ def run(preprocessor, classifier, seed=None):
             stratify=y[mask], random_state=seed
         )
         classifier.fit(X_train, y_train)
-        print(f"Single-subject accuracy ({subject}): {round(classifier.score(X_test, y_test) * 100, 2)}%")
+        y_pred = classifier.predict(X_test)
+        print(f"Single-subject accuracy ({subject}): {round((y_pred == y_test).mean() * 100, 2)}%")
+        ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
 
     # All-subjects accuracy (semi-personalized mode)
     X_train, X_test, y_train, y_test, subjects_train, subjects_test = train_test_split(
@@ -44,12 +47,14 @@ def run(preprocessor, classifier, seed=None):
         stratify=y, random_state=seed
     )
     classifier.fit(X_train, y_train)
-    predicted = classifier.predict(X_test)
-    print(f"All-subject accuracy: {round((predicted == y_test).mean() * 100, 2)}%")
+    y_pred = classifier.predict(X_test)
+    print(f"All-subject accuracy: {round((y_pred == y_test).mean() * 100, 2)}%")
+    ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
     # Break down accuracy by subject
     for subject in range(len(letters)):
         mask = subjects_test == subject
-        print(f"- Subject {subject}: {round((predicted[mask] == y_test[mask]).mean() * 100, 2)}%")
+        print(f"- Subject {subject}: {round((y_pred[mask] == y_test[mask]).mean() * 100, 2)}%")
+        ConfusionMatrixDisplay.from_predictions(y_test[mask], y_pred[mask])
 
     # Left-out-subjects accuracy (new user mode)
     logo = LeaveOneGroupOut()
@@ -57,4 +62,6 @@ def run(preprocessor, classifier, seed=None):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         classifier.fit(X_train, y_train)
-        print(f"Left-out-subject accuracy ({subject}): {round(classifier.score(X_test, y_test) * 100, 2)}%")
+        y_pred = classifier.predict(X_test)
+        print(f"Left-out-subject accuracy ({subject}): {round((y_pred == y_test).mean() * 100, 2)}%")
+        ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
--- strokes1.py
+++ strokes1.py
@@ -9,17 +9,20 @@ import evaluate
 
 
 def filter_transients(x, n):
-    return np.min(x[:len(x)//n*n].reshape((-1, n)), axis=1)
+    return np.median(x[:len(x)//n*n].reshape((-1, n)), axis=1)
 
-def find_strokes(x, verbose=False):
-    X = mlab.specgram(x, NFFT=512, noverlap=256)[0]
-    summed = np.sum(X[15:30], axis=0)
-    filtered = filter_transients(summed, 3)
-    smoothed = np.convolve(filtered, np.ones(6))
+def find_strokes(x, verbose=0):
+    X = mlab.specgram(np.diff(x), NFFT=512, noverlap=256)[0]
+    summed = np.sum(X[:120], axis=0)
+    filtered = summed # filter_transients(summed, 2)
+    smoothed = np.convolve(filtered, np.ones(2))
     smoothed /= np.max(smoothed)
     peaks, info = scipy.signal.find_peaks(smoothed, height=0.2, prominence=0.15, distance=10)
     amp = info["peak_heights"]
     if verbose:
+        if verbose > 1:
+            plt.plot(np.arange(len(summed)) / 2, summed / np.max(summed))
+            plt.plot(filtered / np.max(filtered))
         plt.plot(smoothed)
         plt.scatter(peaks, amp, c='red')
         plt.show()
@@ -75,3 +78,67 @@ if __name__ == '__main__':
     evaluate.run(just_stroke_amps, RadiusNeighborsClassifier(1))
     print("== stroke times & amps ==")
     evaluate.run(stroke_times_and_amps, RadiusNeighborsClassifier(1))
+
+
+
+letters, fs = evaluate.load_dataset()
+
+X = stroke_times_and_amps(letters.reshape(-1), fs)
+y = np.indices(letters.shape)[1].reshape(-1)
+subjects = np.indices(letters.shape)[0].reshape(-1)
+indices = np.indices(letters.shape).reshape((letters.ndim, -1)).T
+
+classifier = RadiusNeighborsClassifier(1)
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(2)
+
+mask = subjects == 0
+X_train, X_test, y_train, y_test, index_train, index_test = train_test_split(
+    X[mask], y[mask], indices[mask],
+    test_size=0.25,
+    stratify=y[mask]
+)
+
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+print(f"{round((y_pred == y_test).mean() * 100, 2)}%")
+for i in range(len(evaluate.alphabet)):
+    mask = y_test == i
+    print(f"- {evaluate.alphabet[i]}: {round((y_pred[mask] == y_test[mask]).mean() * 100, 2)}%")
+ConfusionMatrixDisplay.from_predictions(
+    y_test, y_pred, display_labels=evaluate.alphabet,
+    include_values=True
+)
+
+idx = np.where(y_test == evaluate.alphabet.index('O'))[0]
+idx
+X_test[45]
+evaluate.alphabet[23]
+classifier.predict(X_test[idx])
+index_test[idx]
+X_test[idx].astype(int)
+
+classifier.predict([X_test[108]])
+
+plt.specgram(letters[0, 14, 15], NFFT=512, noverlap=256);
+find_strokes(letters[0, 14, 15], verbose=True)
+
+idx = np.where(y_train == evaluate.alphabet.index('O'))[0]
+index_train[idx]
+find_strokes(letters[0, 14, 5], verbose=True)
+X_train[idx].astype(int)
+
+idx = np.where(y_train == evaluate.alphabet.index('A'))[0]
+index_train[idx]
+
+f, (a, b) = plt.subplots(2)
+X = mlab.specgram(np.diff(letters[0, 0, 0]), NFFT=512, noverlap=511)[0]
+a.imshow(np.log(X), origin='lower', aspect='auto')
+b.plot(X[:120].sum(axis=0))
+f.show()
+
+find_strokes(letters[0, 0, 0], verbose=2)
+X_train[idx].astype(int)
+
+proba = classifier.predict_proba([[0, 1, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000]])[0]
+list(zip(evaluate.alphabet, proba))
