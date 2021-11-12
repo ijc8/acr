ffd8174bd5733c51f42a81297d91a2f9278df782
Ian Clester <ijc@ijc8.me>
2021-11-12 00:34:20 -0500

    DTW with 1D (power) signal.

--- dtw_matcher.py
+++ dtw_matcher.py
@@ -1,104 +1,46 @@
+import math
 import numpy as np
+from dtw import dtw
 
-## A noisy sine wave as query
-idx = np.linspace(0,6.28,num=100)
-query = np.sin(idx) + np.random.uniform(size=100)/10.0
-
-## A cosine is for template; sin and cos are offset by 25 samples
-template = np.cos(idx)
-
-## Find the best match with the canonical recursion formula
-from dtw import *
-help(dtw)
-alignment = dtw(query, template, keep_internals=True)
-
-## Display the warping curve, i.e. the alignment curve
-alignment.plot(type="threeway")
-
-## Align and plot with the Rabiner-Juang type VI-c unsmoothed recursion
-dtw(query, template, keep_internals=True, 
-    step_pattern=rabinerJuangStepPattern(6, "c"))\
-    .plot(type="twoway",offset=-2)
-
-## See the recursion relation, as formula and diagram
-print(rabinerJuangStepPattern(6,"c"))
-rabinerJuangStepPattern(6,"c").plot()
-
-## And much more!
-
-
-from evaluate import load_dataset
-letters, fs = load_dataset()
-
-from matplotlib import mlab
-from scipy import signal
-
-a = mlab.specgram(np.diff(letters[0,0,0]), NFFT=512, noverlap=256)[0].T
-b = mlab.specgram(np.diff(letters[0,0,1]), NFFT=512, noverlap=256)[0].T
-a.shape
-b.shape
-max(signal.correlate(a, b, mode='valid')) / max(len(a), len(b))
-
-from fastdtw import fastdtw
-distance, path = fastdtw(a, b, dist=scipy.spatial.distance.cosine)
-distance
-path
-
-alignment = dtw(a, b, keep_internals=True, dist_method="cosine")
-alignment.plot(type="threeway")
-alignment.distance
-help(alignment)
-alignment.normalizedDistance
-help(alignment.plot)
-help(dtwPlotTwoWay)
-dtwPlotTwoWay(alignment, ts_type="image")
+def block_audio(x, blockSize, hopSize):
+    numBlocks = math.ceil(x.size / hopSize)
+    xb = np.zeros([numBlocks, blockSize])
+    x = np.concatenate((x, np.zeros(blockSize)), axis=0)
+    for n in range(0, numBlocks):
+        i_start = n * hopSize
+        i_stop = np.min([x.size - 1, i_start + blockSize - 1])
+        xb[n][np.arange(0,blockSize)] = x[np.arange(i_start, i_stop + 1)]
+    return xb
 
 def preprocessor(letters, fs):
-    global specgrams
-    specgrams = np.array([
-        mlab.specgram(np.diff(letter), NFFT=512, noverlap=0)[0].T # .sum(axis=1)
-        for letter in letters
-    ], dtype=object)
-    return np.arange(len(specgrams)).reshape(-1, 1)
+    global power
+    power = np.empty(letters.shape, dtype=object)
+    for i, letter in enumerate(letters):
+        letter = letter.astype(float) / np.iinfo(np.int32).max
+        power[i] = np.log((block_audio(np.diff(letter), 2048, 512)**2).sum(axis=1))
+    return np.arange(len(power)).reshape(-1, 1)
 
 def dtw_dist(a, b):
-    a = specgrams[int(a[0])]
-    b = specgrams[int(b[0])]
-    return dtw(a, b, dist_method="cosine").normalizedDistance
-    # return dtw(a, b).normalizedDistance
-    # despite the name, this seems to be much slower...
-    # return fastdtw(a, b, dist=scipy.spatial.distance.cosine)[0]
-
-def cc_dist(a, b):
-    a = specgrams[int(a[0])]
-    b = specgrams[int(b[0])]
-    return np.max(signal.correlate(a, b, mode='valid')) / max(len(a), len(b))
-
-
-cls = KNeighborsClassifier(1, metric=distance, algorithm='brute', n_jobs=8)
-
-cls = KNeighborsClassifier(1, metric=cc_dist, algorithm='brute')
+    a = power[int(a[0])]
+    b = power[int(b[0])]
+    return dtw(a, b, dist_method="euclidean").normalizedDistance
 
 from sklearn.neighbors import KNeighborsClassifier
 cls = KNeighborsClassifier(1, metric=dtw_dist, algorithm='brute', n_jobs=8)
 
-X = preprocessor(letters.reshape(-1), fs)
-y = np.indices(letters.shape)[1].reshape(-1)
-subjects = np.indices(letters.shape)[0].reshape(-1)
-indices = np.indices(letters.shape).reshape((letters.ndim, -1)).T
+import evaluate
+evaluate.run(preprocessor, cls, subset=np.arange(4))
 
-
-from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.model_selection import train_test_split
-mask = subjects == 0
-X_train, X_test, y_train, y_test, index_train, index_test = train_test_split(
-    X[mask], y[mask], indices[mask],
-    test_size=0.25,
-    stratify=y[mask]
-)
-
-cls.fit(X_train, y_train)
-cls.predict([X_test[0]])
-print(y_test[0])
-y_pred = cls.predict(X_test)
-print((y_pred == y_test).mean())
+# Single-subject accuracy (0): 95.0%
+# Single-subject accuracy (1): 90.0%
+# Single-subject accuracy (2): 100.0%
+# Single-subject accuracy (3): 100.0%
+# All-subject accuracy: 97.5%
+# - Subject 0: 100.0%
+# - Subject 1: 94.74%
+# - Subject 2: 95.24%
+# - Subject 3: 100.0%
+# Left-out-subject accuracy (0): 82.5%
+# Left-out-subject accuracy (1): 66.25%
+# Left-out-subject accuracy (2): 78.75%
+# Left-out-subject accuracy (3): 70.0%
